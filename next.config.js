00f66313304dc4982f7c8b86d89b717d2ee4ead2
FAUST00 <fausto.fgj@gmail.com>
2026-06-06 21:28:51 -0400

    Add environment variable for SeedGuard API URL

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_SEEDGUARD_API_URL: "https://seedguard-api.onrender.com",   // ← CHANGE THIS
+  },
 };
 
 module.exports = nextConfig;
