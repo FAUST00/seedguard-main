abd80140a101be2e7565561918f8edbf93090cca
FAUST00 <fausto.fgj@gmail.com>
2026-06-07 22:17:08 -0400

    Update Supabase URL in next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     unoptimized: true,
   },
 env: {
-  NEXT_PUBLIC_SUPABASE_URL: "https://earfjshpbwcnpqjnrnvl.supabase.co",
+  NEXT_PUBLIC_SUPABASE_URL: "https://ezqyfdobdrtosomzpdst.supabase.co",
   NEXT_PUBLIC_SUPABASE_ANON_KEY: "sb_publishable_-VBntNg0Es2VP5tBmURADw_SpMrpAV-",   // ← Get this from Supabase
   NEXT_PUBLIC_SEEDGUARD_API_URL: "https://seedguard-api.onrender.com",
 },
