e5dc905f49b939253a1bd87b84a2a6312a61f4e6
FAUST00 <fausto.fgj@gmail.com>
2026-06-10 21:51:14 -0400

    Remove unused config options from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/seedguard-main',
-  assetPrefix: '/seedguard-main/',
-  trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_SEEDGUARD_API_URL: "https://seedguard-api.onrender.com",   // ← CHANGE THIS
   },
 };
 
